1bcc9171e03e57031ef897a2580252cea82d81b5
Kat Harris <katharrismail@gmail.com>
2020-01-16 12:38:42 -0500

    always

--- bohicket.saltydog.com/daily-special/index.php
+++ bohicket.saltydog.com/daily-special/index.php
@@ -1,6 +1,8 @@
 <?php
-  header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
-header("Cache-Control: no-store");
+  //set headers to NOT cache a page
+  header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+  header("Pragma: no-cache"); //HTTP 1.0
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ?>
 
 
